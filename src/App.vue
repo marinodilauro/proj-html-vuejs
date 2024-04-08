11a8ab56fdeb21eb97bfeb6074e18e3e10e5eaed
marinodilauro <m.dilauro90@gmail.com>
2024-04-08 11:33:47 +0200

    Add header main and footer components files with style files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script>
 
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+
 </script>
 
 <template>
