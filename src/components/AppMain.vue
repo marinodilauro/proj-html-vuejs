f1a0493f8ae63b37ec52807b82f55dd904227460
marinodilauro <m.dilauro90@gmail.com>
2024-04-08 16:38:48 +0200

    Add button class

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,15 +10,19 @@ export default {
 
   <main id="main">
 
-    <div class="banner d-flex align-items-center">
+    <div class="banner d-flex align-items-center justify-content-start">
       <div class="custom-container d-flex flex-column">
+        <div class="col-4">
 
-        <div class="label py-1">
-          <h3 class="oxanium-semibold px-4 m-0">Upcoming Stock</h3>
+          <div class="label py-1">
+            <h3 class="oxanium-semibold px-4 m-0">Upcoming Stock</h3>
+          </div>
+
+          <h1 class="game_title oxanium-semibold">Resident Evil 3 Remake</h1>
+
+          <button class="button poppins-regular" type="button">Shop Now</button>
+
         </div>
-
-        <h1 class="game_title oxanium-semibold">Resident Evil 3 Remake</h1>
-        <button type="button">Shop Now</button>
       </div>
 
     </div>
