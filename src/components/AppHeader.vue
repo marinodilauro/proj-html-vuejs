7a6f808a6d8d0ea08788471ff1d9cad61e8bdd8e
marinodilauro <m.dilauro90@gmail.com>
2024-04-08 13:37:50 +0200

    Add header bottom side

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,24 +9,50 @@ export default {
 <template>
 
   <header class="poppins-regular" id="header">
-    <div class="debug custom-container">
 
-      <div class="info py-2">
 
-        <ul class="d-flex list-unstyled p-0 m-0">
-          <li> <a href="">Whishlist (0)</a></li>
-          <li class="px-3">|</li>
-          <li class="flex-fill">
-            <a href="">Need Help ? </a>
-            <i class="fa-solid fa-chevron-down fa-xs"></i>
-          </li>
-          <li><a href="">Call 24/7 . 800-8754-987</a></li>
-        </ul>
+    <div class="custom-container info">
 
-      </div>
-      <div class="menu"></div>
+      <ul class="d-flex list-unstyled p-0 m-0">
+        <li><a href="">Whishlist (0)</a></li>
+        <li class="px-3">|</li>
+        <li class="flex-fill">
+          <a href="">Need Help ? </a>
+          <i class="fa-solid fa-chevron-down fa-xs"></i>
+        </li>
+        <li><a href="">Call 24/7 . 800-8754-987</a></li>
+      </ul>
 
     </div>
+
+    <hr class="m-0">
+
+    <div class="custom-container menu d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <img src="/public/images/logo.png" alt="">
+      </div>
+
+      <ul class="navbar d-flex list-unstyled play-bold gap-5 p-0 m-0">
+        <li><a href="">Home</a></li>
+        <li><a href="">Shop</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Media</a></li>
+        <li><a href="">ShortCode</a></li>
+        <li><a href="">Features</a></li>
+      </ul>
+
+      <ul class="profile-icons list-unstyled play-bold d-flex gap-4 p-0 m-0">
+        <li><a href=""><i class="fa-solid fa-magnifying-glass"></i></a></li>
+        <li><a href=""><i class="fa-regular fa-user"></i></a></li>
+        <li>
+          <a href=""><i class="fa-solid fa-bag-shopping"></i></a>
+          (0) €0.00
+        </li>
+      </ul>
+
+    </div>
+
   </header>
 
 
